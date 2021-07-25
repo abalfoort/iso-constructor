8a6c24fdfce773d789ec5fea524c2da9d8fc9e7d
abalfoort <arjenbalfoort@hotmail.com>
2021-07-25 15:46:06 +0200

    Upped version

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 __title__ = 'iso-creator'
-__version__ = '3.8.4'
+__version__ = '3.8.7'
 __author__ = 'Arjen Balfoort'
 __author_email__ = 'arjenbalfoort@solydxk.com'
 __maintainer__ = 'Arjen Balfoort'
