4c1c3fba0fcdc7b75ecb82d569230b7bf3813bb5
abalfoort <arjenbalfoort@hotmail.com>
2023-08-14 11:53:27 +0200

    v4.2.3

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 __title__ = 'iso-creator'
-__version__ = '4.2.2'
+__version__ = '4.2.3'
 __author__ = 'Arjen Balfoort'
 __author_email__ = 'arjenbalfoort@hotmail.com'
 __maintainer__ = 'Arjen Balfoort'
